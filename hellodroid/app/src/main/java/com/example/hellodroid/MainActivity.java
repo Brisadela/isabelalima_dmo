9c876aff08cc6326f8060c5c0818f1096dc68265
isabelalima <limadisabela@gmail.com>
2021-04-21 15:53:38 -0300

    Finalização do projeto HelloDroid

---
+++ hellodroid/app/src/main/java/com/example/hellodroid/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.hellodroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mensagemTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mensagemTextView = findViewById(R.id.textview_mensagem);
+        mensagemTextView.setText("Hello Droid");
+    }
+}
